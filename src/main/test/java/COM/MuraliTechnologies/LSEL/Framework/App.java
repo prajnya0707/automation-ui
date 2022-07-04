ca48e61ea3e6ea246ca444acaf6c0fda3e5d150c
prajnya0707 <104439429+prajnya0707@users.noreply.github.com>
2022-07-04 20:42:27 +0530

    remaining

---
+++ src/main/test/java/COM/MuraliTechnologies/LSEL/Framework/App.java
@@ -0,0 +1,13 @@
+package COM.MuraliTechnologies.LSEL.Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
